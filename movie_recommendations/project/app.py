7ade00aada47e1d88e2c1f1b6f8e7076fcca7ff9
vengottip <110312357+vengottip@users.noreply.github.com>
2024-12-14 22:00:33 -0500

    updates

--- movie_recommendations/project/app.py
+++ movie_recommendations/project/app.py
@@ -140,6 +140,7 @@ def index():
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get("PORT", 5000))  # Use PORT environment variable if provided
+    #port = int(os.environ.get("PORT", 5000))  # Use PORT environment variable if provided
+    port = int(os.getenv('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)  # Bind to 0.0.0.0 to allow external access
-    app.run(debug=True)
+    # app.run(debug=True)
